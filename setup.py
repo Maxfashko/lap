18118acca3930e72f9ca53b79e2c2c4c85c3b312
Maxfashko <maxfashko@gmail.com>
2023-02-08 21:13:32 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -172,7 +172,7 @@ def configuration(parent_package='', top_path=None):
 
 def setup_package():
     metadata = dict(name=DISTNAME,
-                    setup_requires=["numpy", "cython"],
+                    setup_requires=["numpy>=1.21.6", "cython>=0.29.32"],
                     maintainer=MAINTAINER,
                     maintainer_email=MAINTAINER_EMAIL,
                     description=DESCRIPTION,
